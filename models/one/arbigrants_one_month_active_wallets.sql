9728165f41c078a84e5e867d80f145254571843f
kofi@arbigrants.com <kofi@arbigrants.com>
2024-05-05 16:17:39 +0000

    commit

--- models/one/arbigrants_one_month_active_wallets.sql
+++ models/one/arbigrants_one_month_active_wallets.sql
@@ -7,28 +7,28 @@
 
 with total AS (
 SELECT 
-TO_VARCHAR(DATE_TRUNC('week',BLOCK_TIMESTAMP), 'YYYY-MM-DD') AS date,
+TO_VARCHAR(DATE_TRUNC('month',BLOCK_TIMESTAMP), 'YYYY-MM-DD') AS date,
 'total' as category,
 COUNT(DISTINCT FROM_ADDRESS) AS active_wallets
 FROM {{ source('arbitrum_raw', 'transactions') }}
-WHERE BLOCK_TIMESTAMP < DATE_TRUNC('week',CURRENT_DATE())
+WHERE BLOCK_TIMESTAMP < DATE_TRUNC('month',CURRENT_DATE())
 {% if is_incremental() %}
-AND BLOCK_TIMESTAMP >= DATE_TRUNC('week',CURRENT_DATE()) - interval '2 week' 
+AND BLOCK_TIMESTAMP >= DATE_TRUNC('month',CURRENT_DATE()) - interval '2 month' 
 {% endif %}
 GROUP BY 1,2
 )
 
 , grantees AS (
 SELECT 
-TO_VARCHAR(DATE_TRUNC('week',BLOCK_TIMESTAMP), 'YYYY-MM-DD') AS date,
+TO_VARCHAR(DATE_TRUNC('month',BLOCK_TIMESTAMP), 'YYYY-MM-DD') AS date,
 'grantees' as category,
 COUNT(DISTINCT FROM_ADDRESS) AS active_wallets
 FROM {{ source('arbitrum_raw', 'transactions') }} t
 INNER JOIN {{ ref('arbigrants_labels_project_contracts') }} c
 ON c.CONTRACT_ADDRESS = t.TO_ADDRESS
-AND BLOCK_TIMESTAMP < DATE_TRUNC('week',CURRENT_DATE())
+AND BLOCK_TIMESTAMP < DATE_TRUNC('month',CURRENT_DATE())
 {% if is_incremental() %}
-AND BLOCK_TIMESTAMP >= DATE_TRUNC('week',CURRENT_DATE()) - interval '2 week' 
+AND BLOCK_TIMESTAMP >= DATE_TRUNC('month',CURRENT_DATE()) - interval '2 month' 
 {% endif %}
 GROUP BY 1,2
 )
