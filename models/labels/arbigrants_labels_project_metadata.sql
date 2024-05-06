6dc3e312ceacf3f12aa53381ee864b40e2540ab2
kofi@arbigrants.com <kofi@arbigrants.com>
2024-05-06 10:15:26 +0000

    commit

--- models/labels/arbigrants_labels_project_metadata.sql
+++ models/labels/arbigrants_labels_project_metadata.sql
@@ -4,8 +4,8 @@
 )
 }}
 
-SELECT name, category, grant_date, llama_slug, llama_name, description
+SELECT name, category, grant_date, llama_slug, llama_name, chain, description
 FROM (VALUES
-('Example1', 'yield', '2024-01-01', 'hmx', 'HMX', 'Lorem ipsum lo menos'), 
-('Example2', 'derivatives', '2024-01-01', 'pendle', 'Pendle', 'Lorem ipsum lo menos')
+('Example1', 'yield', '2024-01-01', 'hmx', 'HMX', 'Arbitrum One', 'Lorem ipsum lo menos'), 
+('Example2', 'derivatives', '2024-01-01', 'pendle', 'Pendle', 'Arbitrum One', 'Lorem ipsum lo menos')
 ) AS x (name, category, grant_date, llama_slug, llama_name, description)
