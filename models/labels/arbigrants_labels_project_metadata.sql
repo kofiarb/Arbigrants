debe0703f530416e35fd33bccc8c18f322c5e6e6
kofi@arbigrants.com <kofi@arbigrants.com>
2024-05-09 16:24:59 +0000

    commit

--- models/labels/arbigrants_labels_project_metadata.sql
+++ models/labels/arbigrants_labels_project_metadata.sql
@@ -8,4 +8,4 @@ SELECT name, category, grant_date, llama_slug, llama_name, chain, description
 FROM (VALUES
 ('Example1', 'yield', '2024-01-01', 'hmx', 'HMX', 'Arbitrum One', 'Lorem ipsum lo menos'), 
 ('Example2', 'derivatives', '2024-01-01', 'pendle', 'Pendle', 'Arbitrum One', 'Lorem ipsum lo menos')
-) AS x (name, category, grant_date, llama_slug, llama_name, description)
+) AS x (name, category, grant_date, llama_slug, llama_name, chain, description)
--- models/one/arbigrants_one_active_wallet_summary.sql
+++ models/one/arbigrants_one_active_wallet_summary.sql
@@ -6,11 +6,14 @@
 
 WITH actives_24h AS (
 SELECT COUNT(DISTINCT FROM_ADDRESS) as day_active_wallets
-FROM ARBITRUM.RAW.TRANSACTIONS t
-INNER JOIN ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_CONTRACTS c
+FROM {{ source('arbitrum_raw', 'transactions') }} t
+INNER JOIN {{ ref('arbigrants_labels_project_contracts') }} c
 ON c.CONTRACT_ADDRESS = t.TO_ADDRESS
 AND BLOCK_TIMESTAMP < CURRENT_DATE
 AND BLOCK_TIMESTAMP >= CURRENT_DATE - interval '1 day'
+INNER JOIN {{ ref('arbigrants_labels_project_metadata') }} m
+ON m.NAME = c.NAME 
+AND m.chain = 'Arbitrum One'
 ),
 
 actives_growth_24h AS (
@@ -18,10 +21,13 @@ WITH active_wallet_counts AS (
   SELECT
       COUNT(DISTINCT CASE WHEN BLOCK_TIMESTAMP >= CURRENT_DATE - interval '1 day' AND BLOCK_TIMESTAMP < CURRENT_DATE THEN FROM_ADDRESS END) as past_day_wallets,
       COUNT(DISTINCT CASE WHEN BLOCK_TIMESTAMP < CURRENT_DATE - interval '1 day' AND BLOCK_TIMESTAMP >= CURRENT_DATE - interval '2 day' THEN FROM_ADDRESS END) as day_before_wallets
-  FROM ARBITRUM.RAW.TRANSACTIONS t
-  INNER JOIN ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_CONTRACTS c
+  FROM {{ source('arbitrum_raw', 'transactions') }} t
+  INNER JOIN {{ ref('arbigrants_labels_project_contracts') }} c
   ON c.CONTRACT_ADDRESS = t.TO_ADDRESS
   AND BLOCK_TIMESTAMP >= CURRENT_DATE - interval '2 day'
+  INNER JOIN {{ ref('arbigrants_labels_project_metadata') }} m
+  ON m.NAME = c.NAME 
+  AND m.chain = 'Arbitrum One'
 )
 SELECT
   ROUND((100 * (past_day_wallets / NULLIF(day_before_wallets, 0)) - 100), 1) AS day_growth
@@ -30,11 +36,14 @@ FROM active_wallet_counts
 
 actives_7d AS (
 SELECT COUNT(DISTINCT FROM_ADDRESS) as week_active_wallets
-FROM ARBITRUM.RAW.TRANSACTIONS t
-INNER JOIN ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_CONTRACTS c
+FROM {{ source('arbitrum_raw', 'transactions') }} t
+INNER JOIN {{ ref('arbigrants_labels_project_contracts') }} c
 ON c.CONTRACT_ADDRESS = t.TO_ADDRESS
 AND BLOCK_TIMESTAMP < CURRENT_DATE
 AND BLOCK_TIMESTAMP >= CURRENT_DATE - interval '7 day'
+INNER JOIN {{ ref('arbigrants_labels_project_metadata') }} m
+ON m.NAME = c.NAME 
+AND m.chain = 'Arbitrum One'
 ),
 
 actives_growth_7d AS (
@@ -42,10 +51,13 @@ WITH active_wallet_counts AS (
   SELECT
       COUNT(DISTINCT CASE WHEN BLOCK_TIMESTAMP >= CURRENT_DATE - interval '7 day' AND BLOCK_TIMESTAMP < CURRENT_DATE THEN FROM_ADDRESS END) as past_day_wallets,
       COUNT(DISTINCT CASE WHEN BLOCK_TIMESTAMP < CURRENT_DATE - interval '7 day' AND BLOCK_TIMESTAMP >= CURRENT_DATE - interval '14 day' THEN FROM_ADDRESS END) as day_before_wallets
-  FROM ARBITRUM.RAW.TRANSACTIONS t
-  INNER JOIN ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_CONTRACTS c
+  FROM {{ source('arbitrum_raw', 'transactions') }} t
+  INNER JOIN {{ ref('arbigrants_labels_project_contracts') }} c
   ON c.CONTRACT_ADDRESS = t.TO_ADDRESS
   AND BLOCK_TIMESTAMP >= CURRENT_DATE - interval '14 day'
+  INNER JOIN {{ ref('arbigrants_labels_project_metadata') }} m
+  ON m.NAME = c.NAME 
+  AND m.chain = 'Arbitrum One'
 )
 SELECT
   ROUND((100 * (past_day_wallets / NULLIF(day_before_wallets, 0)) - 100), 1) AS week_growth
@@ -54,11 +66,14 @@ FROM active_wallet_counts
 
 actives_1m AS (
 SELECT COUNT(DISTINCT FROM_ADDRESS) as month_active_wallets 
-FROM ARBITRUM.RAW.TRANSACTIONS t
-INNER JOIN ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_CONTRACTS c
+FROM {{ source('arbitrum_raw', 'transactions') }} t
+INNER JOIN {{ ref('arbigrants_labels_project_contracts') }} c
 ON c.CONTRACT_ADDRESS = t.TO_ADDRESS
 AND BLOCK_TIMESTAMP < CURRENT_DATE
 AND BLOCK_TIMESTAMP >= CURRENT_DATE - interval '1 month'
+INNER JOIN {{ ref('arbigrants_labels_project_metadata') }} m
+ON m.NAME = c.NAME 
+AND m.chain = 'Arbitrum One'
 ),
 
 actives_growth_1m AS (
@@ -66,10 +81,13 @@ WITH active_wallet_counts AS (
   SELECT
       COUNT(DISTINCT CASE WHEN BLOCK_TIMESTAMP >= CURRENT_DATE - interval '1 month' AND BLOCK_TIMESTAMP < CURRENT_DATE THEN FROM_ADDRESS END) as past_day_wallets,
       COUNT(DISTINCT CASE WHEN BLOCK_TIMESTAMP < CURRENT_DATE - interval '1 month' AND BLOCK_TIMESTAMP >= CURRENT_DATE - interval '2 month' THEN FROM_ADDRESS END) as day_before_wallets
-  FROM ARBITRUM.RAW.TRANSACTIONS t
-  INNER JOIN ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_CONTRACTS c
+  FROM {{ source('arbitrum_raw', 'transactions') }} t
+  INNER JOIN {{ ref('arbigrants_labels_project_contracts') }} c
   ON c.CONTRACT_ADDRESS = t.TO_ADDRESS
   AND BLOCK_TIMESTAMP >= CURRENT_DATE - interval '2 month'
+  INNER JOIN {{ ref('arbigrants_labels_project_metadata') }} m
+  ON m.NAME = c.NAME 
+  AND m.chain = 'Arbitrum One'
 )
 SELECT
   ROUND((100 * (past_day_wallets / NULLIF(day_before_wallets, 0)) - 100), 1) AS month_growth
