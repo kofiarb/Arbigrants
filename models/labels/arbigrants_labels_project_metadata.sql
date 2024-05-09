d601430514e1bbbc5e81b37ede3086820b5660b1
kofi@arbigrants.com <kofi@arbigrants.com>
2024-05-09 17:00:35 +0000

    commit

--- models/labels/arbigrants_labels_project_metadata.sql
+++ models/labels/arbigrants_labels_project_metadata.sql
@@ -6,6 +6,6 @@
 
 SELECT name, category, grant_date, llama_slug, llama_name, chain, description, logo
 FROM (VALUES
-('Example1', 'yield', '2024-01-01', 'hmx', 'HMX', 'Arbitrum One', 'Lorem ipsum lo menos', 'https://i.imgur.com/EALllxl.png'), 
-('Example2', 'derivatives', '2024-01-01', 'pendle', 'Pendle', 'Arbitrum One', 'A vertically-integrated DEX on Arbitrum featuring spot, perpetual, and integrated money markets. Universal cross-margin with lightning-fast performance. Take back control.', 'https://i.imgur.com/EALllxl.png')
+('Example Project 1', 'yield', '2024-01-01', 'hmx', 'HMX', 'Arbitrum One', 'Lorem ipsum lo menos', 'https://i.imgur.com/EALllxl.png'), 
+('Example Project 2', 'derivatives', '2024-01-01', 'pendle', 'Pendle', 'Arbitrum One', 'A vertically-integrated DEX on Arbitrum featuring spot, perpetual, and integrated money markets. Universal cross-margin with lightning-fast performance. Take back control.', 'https://i.imgur.com/EALllxl.png')
 ) AS x (name, category, grant_date, llama_slug, llama_name, chain, description, logo)
