1ef848967e26b56b02e8f757432e884f955d99c2
kofi@arbigrants.com <kofi@arbigrants.com>
2024-05-10 12:23:37 +0000

    commit

--- models/labels/arbigrants_labels_project_metadata.sql
+++ models/labels/arbigrants_labels_project_metadata.sql
@@ -4,8 +4,8 @@
 )
 }}
 
-SELECT name, category, grant_date, llama_slug, llama_name, chain, description, logo
+SELECT name, category, grant_date, llama_slug, chain, description, logo
 FROM (VALUES
-('Example Project 1', 'yield', '2024-01-01', 'eigenlayer-protocols-part', 'https://defillama.com/protocol/radiant#informations two', 'Arbitrum One', 'Lorem ipsum lo menos', 'https://i.imgur.com/EALllxl.png'), 
-('Example Project 2', 'derivatives', '2024-01-01', 'eigenlayer-protocols-part', 'Eigenlayers protocls part 7', 'Arbitrum One', 'A vertically-integrated DEX on Arbitrum featuring spot, perpetual, and integrated money markets. Universal cross-margin with lightning-fast performance. Take back control.', 'https://i.imgur.com/EALllxl.png')
-) AS x (name, category, grant_date, llama_slug, llama_name, chain, description, logo)
+('Example Project 1', 'yield', '2024-01-01', 'eigenlayer-protocols-part', 'Arbitrum One', 'Lorem ipsum lo menos', 'https://i.imgur.com/EALllxl.png'), 
+('Example Project 2', 'derivatives', '2024-01-01', 'eigenlayer-protocols-part', 'Arbitrum One', 'A vertically-integrated DEX on Arbitrum featuring spot, perpetual, and integrated money markets. Universal cross-margin with lightning-fast performance. Take back control.', 'https://i.imgur.com/EALllxl.png')
+) AS x (name, category, grant_date, llama_slug, chain, description, logo)
