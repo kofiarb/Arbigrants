d1372685e9eac322f28caabfa9daec554e25dba4
Kofi Arb <kofi@arbigrants.com>
2024-05-29 11:38:18 +0000

    commit

--- models/one/arbigrants_one_day_leaderboard.sql
+++ models/one/arbigrants_one_day_leaderboard.sql
@@ -21,13 +21,18 @@ aggregated_data AS (
         COUNT(DISTINCT CASE WHEN t.BLOCK_TIMESTAMP >= ts.one_period_ago AND t.BLOCK_TIMESTAMP < CURRENT_DATE THEN t.FROM_ADDRESS END) AS active_accounts_current,
         COUNT(DISTINCT CASE WHEN t.BLOCK_TIMESTAMP < ts.one_period_ago AND t.BLOCK_TIMESTAMP >= ts.two_period_ago THEN t.FROM_ADDRESS END) AS active_accounts_previous,
         SUM(CASE WHEN t.BLOCK_TIMESTAMP >= ts.one_period_ago THEN ((t.RECEIPT_EFFECTIVE_GAS_PRICE * t.RECEIPT_GAS_USED)/1e18) END) AS gas_spend_current,
-        SUM(CASE WHEN t.BLOCK_TIMESTAMP < ts.one_period_ago AND t.BLOCK_TIMESTAMP >= ts.two_period_ago THEN ((t.RECEIPT_EFFECTIVE_GAS_PRICE * t.RECEIPT_GAS_USED)/1e18) END) AS gas_spend_previous
+        SUM(CASE WHEN t.BLOCK_TIMESTAMP < ts.one_period_ago AND t.BLOCK_TIMESTAMP >= ts.two_period_ago THEN ((t.RECEIPT_EFFECTIVE_GAS_PRICE * t.RECEIPT_GAS_USED)/1e18) END) AS gas_spend_previous,
+        h.TOTAL_LIQUIDITY_USD AS TVL
     FROM {{ source('arbitrum_raw', 'transactions') }} t  
     INNER JOIN ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_CONTRACTS l
     ON t.TO_ADDRESS = l.CONTRACT_ADDRESS
     INNER JOIN ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_METADATA m
     ON m.NAME = l.NAME 
     AND m.CHAIN = 'Arbitrum One'
+    LEFT JOIN DEFILLAMA.TVL.HISTORICAL_TVL_PER_CHAIN h
+    ON h.CHAIN = 'Arbitrum'
+    AND h.DATE = current_date
+    AND h.PROTOCOL_NAME = LLAMA_NAME
     CROSS JOIN time_settings ts
     WHERE t.BLOCK_TIMESTAMP >= ts.two_period_ago
     GROUP BY 1,2,3,4
@@ -52,6 +57,7 @@ ad.active_accounts_current as WALLETS,
 CASE 
     WHEN ad.active_accounts_previous > 0 THEN (100 * (ad.active_accounts_current - ad.active_accounts_previous) / ad.active_accounts_previous) 
     ELSE 0 
-END as WALLETS_GROWTH
+END as WALLETS_GROWTH,
+tvl
 FROM aggregated_data ad  
 ORDER BY ad.gas_spend_current DESC
