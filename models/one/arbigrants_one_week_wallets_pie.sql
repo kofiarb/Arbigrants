e2721998954774fa8f406fa4b76341efc2c8738b
Kofi Arb <kofi@arbigrants.com>
2024-06-14 16:24:08 +0000

    commit

---
+++ models/one/arbigrants_one_week_wallets_pie.sql
@@ -0,0 +1,34 @@
+{{ config
+(
+    materialized = 'table'
+)
+}}
+
+WITH cte AS (
+SELECT 
+c.NAME,
+COUNT(DISTINCT FROM_ADDRESS) AS active_wallets,
+SUM(COUNT(DISTINCT FROM_ADDRESS)) OVER () AS total_wallets
+FROM {{ source('arbitrum_raw', 'transactions') }} t
+INNER JOIN ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_CONTRACTS c
+ON c.CONTRACT_ADDRESS = t.TO_ADDRESS
+AND BLOCK_TIMESTAMP < CURRENT_DATE
+AND BLOCK_TIMESTAMP >= CURRENT_DATE - interval '7 day'
+GROUP BY 1
+),
+ranked_cte AS (
+  SELECT 
+    NAME,
+    active_wallets,
+    total_wallets,
+    ROUND(active_wallets / total_wallets * 100, 2) AS PCT_TVL,
+    RANK() OVER (ORDER BY active_wallets DESC) AS rnk
+  FROM cte
+)
+SELECT 
+  CASE WHEN rnk <= 5 THEN NAME ELSE 'Other' END AS NAME,
+  SUM(active_wallets) AS active_wallets,
+  ROUND(SUM(active_wallets) / MAX(total_wallets) * 100, 2) AS PCT_WALLETS
+FROM ranked_cte
+GROUP BY CASE WHEN rnk <= 5 THEN NAME ELSE 'Other' END
+ORDER BY active_wallets DESC
